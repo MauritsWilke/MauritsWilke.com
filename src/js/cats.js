308eff5ec426b924fc7a52b7a09fcc0eaca1add4
Maurits Wilke <mauritswilke@gmail.com>
2021-12-17 22:00:41 +0100

    Update but this time with working cats

--- src/js/cats.js
+++ src/js/cats.js
@@ -3,7 +3,8 @@ const slideshows = [...document.getElementsByClassName("imageSlideshow")]
 setInterval(async () => {
 	for (image in slideshows) {
 		let imageCount = slideshows[image].children[0].children[0].src.toLowerCase().match(/\d*\.jpg/)[0].replace(".jpg", "")
-		if (imageCount == 9) imageCount = 1
+		if (+imageCount == 9) imageCount = 0
+		imageCount++
 		slideshows[image].children[0].children[0].src = `../images/${image == 0 ? "pippa" : "olly"}/${imageCount}.jpg`
 	}
 }, 5000)
